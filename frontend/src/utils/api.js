1c971a890d109b9c860250cd7a8765b3c4ea2de0
Vladislav Kireev <kireew.vladislaw@gmail.com>
2023-07-04 20:44:47 +0300

    feature: added ecosystem and env files

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
 }
 // Замените на адрес вашего бэкенда
-const api = new Api('http://localhost:4000');
+const api = new Api('https://api.vladislav.nomoreparties.sbs');
 
 export default api;
